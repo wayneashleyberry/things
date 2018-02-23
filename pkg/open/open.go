f39a7f6019a0552526f90a194ab5a9f76f721be4
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2018-02-23 10:24:08 +0200

    removes debug statement

--- pkg/open/open.go
+++ pkg/open/open.go
@@ -8,7 +8,6 @@ import (
 // Open wraps the shell command "open", considering Things is only available
 // on macOS this should probably be fine.
 func Open(url string) {
-	log.Println(url)
 	cmd := exec.Command("open", url)
 	err := cmd.Run()
 	if err != nil {
